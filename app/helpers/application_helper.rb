192abead2530a22deadb9bb3bbe3fd1caa838507
Jose Donizetti <jdbjunior@gmail.com>
2011-03-26 17:32:55 -0300

    helper which you can see tweets filtered by hashtag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'rubygems'
+require 'twitter'
+
 module ApplicationHelper
+
+	def tweets(hash_tag="reclamoes")
+		search = Twitter::Search.new
+		result = search.hashtag(hash_tag);
+		result.to_a
+	end
+
+
 end
