cefc7afa3e4dc19bd207f1af8e057bd647ddccce
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2011-03-26 16:40:49 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reclamoes::Application.config.secret_token = 'aeb26c860b7201550db9ca4cef751821bc66c99056fe0f151912cab53171ef3974b777647bc61730112762b9b3bb8247e9e618e9b6701156cae84f756f99d204'
